715673f5250568bf9f9c22dd43b9c42f6ea72288
Woman Developer <womandeveloper@outlook.com>
2020-12-17 01:05:40 +0300

    new updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,18 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
-    protected  $guarded = [];
-
-    public function categories(){
-        return $this->belongsToMany('App\Models\Category','category_product');
-    }
-
-    public function detail(){
-        return $this->hasOne('App\Models\ProductDetail');
-    }
+    protected $table = 'product';
+    protected $guarded =  [];
 }
