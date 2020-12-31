c29affb61b7da17f78f1d85fc611af5a64986cb5
Woman Developer <womandeveloper@outlook.com>
2020-12-31 06:32:18 +0300

    Dashboard files added

--- app/Http/Controllers/backend/HomeController.php
+++ app/Http/Controllers/backend/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('backend.index');
+        $waiting_order = Order::where('status','Siparişiniz alındı')->count();
+        return view('backend.index', compact('waiting_order'));
     }
 }
